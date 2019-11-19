60e1c5ddbdb107b28d24c4bc91d38e3e9d501767
IvanPalm <ivan.palmegiani@gmail.com>
2019-11-19 16:57:10 +0100

    scraping loop functional, missing url generator

--- check24_scraper.py
+++ check24_scraper.py
@@ -1,11 +1,18 @@
-url = 'https://vergleich.check24.de/common/ajax/tariff_detail?&calculationparameterId=1845c5f5f696a77e002894ca988b51ef&tariffversionId=936998&tariffversionVariationKey=b-325169-b-875205-b-875206-b-875207&productId=1&c24_reference_tariff=no'
-#url2 = 'https://vergleich.check24.de/common/ajax/tariff_detail?&calculationparameterId=1845c5f5f696a77e002894ca988b51ef&tariffversionId=936037&tariffversionVariationKey=b-874223-b-874224-b-874225&productId=1&c24_reference_tariff=no'
+url_api = ['https://vergleich.check24.de/common/ajax/tariff_detail?&calculationparameterId=2fc32fbf4ea74b9693cd1d7da2a0246a&tariffversionId=937662&tariffversionVariationKey=b-325169-b-875682-b-875683-b-875684&productId=1&c24_reference_tariff=no',
+'https://vergleich.check24.de/common/ajax/tariff_detail?&calculationparameterId=2fc32fbf4ea74b9693cd1d7da2a0246a&tariffversionId=936037&tariffversionVariationKey=b-874223-b-874224-b-874225&productId=1&c24_reference_tariff=no']
 
 import requests
 from requests.exceptions import HTTPError
+from scrapy import Selector
+import pandas as pd
 
-for url in [url]:
+df = pd.DataFrame(columns = range(0, 13))
+colnames = []
+values = []
+
+for url in url_api:
     try:
+        # resp = requests.get(url + url_par)
         resp = requests.get(url)
 
         # If the response was successful, no Exception will be raised
@@ -17,11 +24,16 @@ for url in [url]:
     else:
         print('Success!')
 
-# resp.encoding
-# resp.text
-# resp.headers
+    resp.encoding = 'utf-8'
+    html = resp.content
+    sel = Selector(text=html)
+    col = sel.xpath('.//div[contains(@class, "fact__label")]')
+    df.columns = ['Anbieter']+[str.strip().rstrip().replace('\\n', '') for str in col.xpath('./text()').extract()]
+    prov = sel.xpath('.//div[contains(@class, "ajax-provider")]')
+    provider = prov.xpath('.//text()').extract()[1].strip().rstrip().replace('\\n', '')
+    val = sel.xpath('.//div[contains(@class, "fact__value")]')
+    values = [str.strip().rstrip().replace('\\n', '') for str in val.xpath('./text()').extract()]
+    new_row = [provider] + values
+    df.loc[len(df) + 1, :] = new_row
 
-from bs4 import BeautifulSoup
-
-soup = BeautifulSoup(resp.text, 'html.parser')
-soup.find_all(class_ = "flex__item--8")
+df
